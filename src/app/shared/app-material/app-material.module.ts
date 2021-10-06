296febd1d500af1b787c0d0df2ea7c0c44d81484
pedrorhamon <pedrorhamon16@gmail.com>
2021-10-05 23:44:58 -0300

    eliminando espaço

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -3,8 +3,6 @@ import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-import { CoursesComponent } from './../../courses/courses/courses.component';
-
 
 
 @NgModule({
